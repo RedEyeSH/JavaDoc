58f00ba6cc2311d16310f7b74df24ec8ab70de84
Than Ngoc Quang <97534002+RedEyeSH@users.noreply.github.com>
2025-03-15 22:55:21 +0200

    JavaDoc about Restaurant Simulator

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"dao"},{"l":"datasource"},{"l":"entity"},{"l":"model"},{"l":"view"}];updateSearchResults();
